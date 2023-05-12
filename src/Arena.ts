974744c46de5ccc443adda571a09ba8832c2679e
crisxh <76125739+crisxh@users.noreply.github.com>
2023-05-12 17:53:48 +0200

    refactored GridSquare class

--- src/Arena.ts
+++ src/Arena.ts
@@ -1,18 +1,6 @@
 
 
-export class GridSquare{
-posX:number
-posY:number
-
-    constructor(posx:number,posy:number){
-        this.posX=posx
-        this.posY=posy
-    }
-    draw(){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.posX, this.posY, 100, 100);
-    }
-}
+export 
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -13,20 +13,24 @@ ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke(); 
 
+class Boundary{
+  constructor(){
+
+  }
+}
+
 class GridSquare{
   posX:number
   posY:number
-  index1:string
-  index2:string
+  squareID:string
   height:number
   width:number
-      constructor(posx:number,posy:number,i:string,j:string){
-          this.posX=posx
-          this.posY=posy
-          this.index1=i
-          this.index2=j
+      constructor(posX:number,posY:number,squareID:string){
+          this.posX=posX
+          this.posY=posY
           this.height=50
           this.width=50;
+          this.squareID=squareID
       }
       draw(){
           ctx.fillStyle = 'green';
